d943ed6310df9441739d7582fa527bed18f7741a
MasterChief8 <john117.696969@gmail.com>
2025-04-08 11:29:18 +0530

    Update script.js
    so now i can do stuff make posts

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 // script.js
 
+
+//here is firebase code
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyABNCC6L7Tcdy1N4P10TEVvFQSEkYq5wQE",
+    authDomain: "blog-website-df667.firebaseapp.com",
+    projectId: "blog-website-df667",
+    storageBucket: "blog-website-df667.firebasestorage.app",
+    messagingSenderId: "604952664067",
+    appId: "1:604952664067:web:a8512bc34710d97792d636",
+    measurementId: "G-LTFQ0TGDRJ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+//end
+
 // Load posts from localStorage
 function loadPosts() {
   const posts = JSON.parse(localStorage.getItem('posts')) || [];
